acb281ce34bdd620e44317a063b8d0f32a31f122
Don Luchini <don-code@users.noreply.github.com>
2020-03-08 23:18:46 -0400

    Extra test for C libraries which return partially complete February 31st's.

--- clock.c
+++ clock.c
@@ -42,56 +42,55 @@ void drawCalendar(struct tm * timeinfo) {
 	wk1 = 0;
 	wk_offset = 0;
 	day_offset = 0;
-	
+
 	/* Print a calendar header. */
 	gotoxy(3,2);
 	strftime(buf, 15, "%B %Y",timeinfo);
 	printf("%s Calendar", buf);
 	gotoxy(3,4);
 	printf("Su Mo Tu We Th Fr Sa  Wk");
-	
+
 	/* Blank out the calendar area. */
 	for(i=0; i < 8; i++) {
 		gotoxy(3, 5+i);
 		printf("                        ");
 	}
-	
+
 	for(i=1; i < 32; i++) { /* Iterate over all possible days in the month. */
 		timeinfo->tm_mday = i;
-		mktime(timeinfo);
-		
-		/* If it's the first day of the month, calculate the start position. */
-		if(i==1) {
-			day_offset = timeinfo->tm_wday;
-			strftime(buf, 3, "%U", timeinfo);
-			wk1 = atoi(buf);
-			gotoxy(25, 5);
-			printf("%d",wk1);
-			
-		/* If it's any other Sunday, with iteration still in the same month. */
-		} else if(timeinfo->tm_wday == 0 && month == timeinfo->tm_mon) {
-			/* Calculate what week it is. */
-			wk_offset++;
-			day_offset = 0;
-			gotoxy(25, 5 + wk_offset);
-			printf("%d",wk1+wk_offset);
-		}
-		
-		/* Print out a single day in the correct position, provided that day
-           exists in the month (prevents Feb. 30, etc.) */
-		if(month == timeinfo->tm_mon) {
-			/* Invert console colors to mark the current day. */
-			if(i==today) {
-				invertColors();
+		if (mktime(timeinfo) != -1) { /* mktime returns -1 for Feb 30 and friends */
+			/* If it's the first day of the month, calculate the start position. */
+			if(i==1) {
+				day_offset = timeinfo->tm_wday;
+				strftime(buf, 3, "%U", timeinfo);
+				wk1 = atoi(buf);
+				gotoxy(25, 5);
+				printf("%d",wk1);
+
+			/* If it's any other Sunday, with iteration still in the same month. */
+			} else if(timeinfo->tm_wday == 0 && month == timeinfo->tm_mon) {
+				/* Calculate what week it is. */
+				wk_offset++;
+				day_offset = 0;
+				gotoxy(25, 5 + wk_offset);
+				printf("%d",wk1+wk_offset);
 			}
-			
-			gotoxy((day_offset * 3)+3,wk_offset + 5);
-			printf("%d",timeinfo->tm_mday);
-			day_offset++;
-			
-			/* Un-invert colors of the current day. */
-			if(i==today) {
-				restoreColors();
+
+			/* Print out a single day in the correct position */
+			if(month == timeinfo->tm_mon) {
+				/* Invert console colors to mark the current day. */
+				if(i==today) {
+					invertColors();
+				}
+
+				gotoxy((day_offset * 3)+3,wk_offset + 5);
+				printf("%d",timeinfo->tm_mday);
+				day_offset++;
+
+				/* Un-invert colors of the current day. */
+				if(i==today) {
+					restoreColors();
+				}
 			}
 		}
 	}
@@ -123,18 +122,18 @@ int main() {
 
 		/* Draw the time on every update. */
 		drawTime(timeinfo);
-		
+
 		/* If the date has changed, draw the date and year. This avoids console
 		   shearing on older hardware. */
 		if(day != timeinfo->tm_mday) {
 			/* Update the day. */
 			day = timeinfo->tm_mday;
 			drawDate(timeinfo);
-			
+
 			/* Update the calendar. */
 			drawCalendar(timeinfo);
 		}
-		
+
 		fflush(stdout);
 		sleep(1);
 	}
